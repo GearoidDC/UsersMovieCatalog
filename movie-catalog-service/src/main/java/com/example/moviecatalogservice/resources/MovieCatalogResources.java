3ced1031f63009e8e389cea669b83a678f54258f
Gearoid de Cleir <gdchot@hotmail.com>
2020-05-07 03:45:12 +0100

    Added Eureka Server

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResources.java
@@ -4,11 +4,13 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Rating;
 import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,28 +24,42 @@ public class MovieCatalogResources {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+    // Get all rated movie IDs
+    UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating( "1234", 4),
-                new Rating("5678" , 3)
-
-        );
-
-        return ratings.stream().map(rating -> {
+        return ratings.getUserRating().stream().map(rating -> {
+            // For each movie ID, call movie info service and get details
             Movie movie = restTemplate.getForObject( "http://localhost:8082/movies/" + rating.getMovieId(),Movie.class);
+
+            // Put them all together
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
 
         })
                 .collect(Collectors.toList());
 
-        // Get all rated movie IDs
 
-        // For each movie ID, call movie info service and get details
 
-        // Put them all together
+
+
+
 
 
     }
 }
+
+
+/*
+            // Webclient is going to be replace restTemplate
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+
+*/
